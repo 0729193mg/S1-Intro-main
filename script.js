d4cd3619d06753b9e0c67de173ec7be4948d641d
LibelluleRose <0729193@cegepoutaouais.qc.ca>
2023-01-11 16:09:19 -0500

    Ajout des modif de la prof

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Bonjour");
+function ajouter() {
+
+    var item = document.getElementById("desc").value;
+    document.getElementById("maListe").append(item); //Inscrit dans la page "item" après avoir clické sur le bouton ajouter
+    alert("L'item est bien ajouté") //Créer un Pop-up d'alerte
+}
+
+
+
