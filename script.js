53e502b3b5607963288354e10e1af6ebfd2efb9e
LibelluleRose <0729193@cegepoutaouais.qc.ca>
2023-01-16 11:58:23 -0500

    faire fonctionner l'ajout d'item avec le prix et le total doit vérifier le code avec celui du prof. hay encore des modif à faire

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
 function ajouter() {
-
+    //va chercher ds ID "desc" la valeur et la donner à la variable item
     var item = document.getElementById("desc").value;
-    document.getElementById("maListe").append(item); //Inscrit dans la page "item" après avoir clické sur le bouton ajouter
-    alert("L'item est bien ajouté") //Créer un Pop-up d'alerte
+
+    //Créer une balise <li>
+    let nouveauLi = document.createElement("li");
+
+    //Créer une variable pour la valeur et lui donner la valeur de l'ID "valeur"
+    let prix = document.getElementById("valeur").value;
+
+    //Écrire dans le paragraphe
+    nouveauLi.innerText = item;
+
+    //va mettre "nouveauLi" ds le ID "maListe" à la fin de la liste -- Ajouter le paragraphe à la ligne
+    document.getElementById("maListe").append(nouveauLi);
+
+    //mettre la variable "valeur" dans le li de l'item. --> nouveauLi
+    nouveauLi.innerText = item + " : " + prix + " $ ";
+
+    //créer un span sur la valeur monétaire
+    let nouveauSpan = document.createElement("span");
+
+    nouveauLi.innerTest = item + " : "
+    nouveauSpan.innerTest = prix + " $ "
+
+    document.getElementById("maListe").append(nouveauLi);
+
+
+    //Mettre à jour le total
+
+    let ancienPrix = document.getElementById("Total").innerText;
+    let nouveauPrix = +ancienPrix + +prix;  //+varible : convertir en numérique
+
+
 }
+
+
+//Inscrit dans la page "item" après avoir clické sur le bouton ajouter
+document.getElementById("maListe").append(item);
+
+//Créer un Pop-up d'alerte
+alert("L'item est bien ajouté")
+
+
+
+
+
+
+
+
 
 
 
