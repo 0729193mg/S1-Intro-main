a52140027af534ac2f30017ba6c211997381b4e9
LibelluleRose <0729193@cegepoutaouais.qc.ca>
2023-01-16 10:00:09 -0500

    modif couleur et enlignement

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ function ajouter() {
 
     var item = document.getElementById("desc").value;
     document.getElementById("maListe").append(item); //Inscrit dans la page "item" après avoir clické sur le bouton ajouter
-    alert("L'item est bien ajouté") //Créer un Pop-up d'alerte
-}
+    alert("L'item est bien ajouté") }//Créer un Pop-up d'alerte
 
+    /*let nombre = -1;
 
+    if (nombre < 0)
+    {console.log("Erreur") }
+    else if (nombre > 100)
+    {console.log ("Erreur") }
 
+else {console.log("Bravo!")}
+
+}/*
+
+*/
